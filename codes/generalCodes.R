b7c0e3174be62a9399876fa74b5c8867be8d1f18
Sudarshan <sudarshanshetty9@gmail.com>
2021-12-16 17:14:40 +0100

    up

--- codes/generalCodes.R
+++ codes/generalCodes.R
@@ -59,6 +59,16 @@ beta_disp <- function(x, group="condition_status", index="bray"){
   bet_di_anova3a
 }
 
+#################################################
+
+get_taxa_tibble <- function(x){
+  tax_table(x) %>% 
+    as.matrix() %>% 
+    as.data.frame() %>% 
+    tibble::rownames_to_column("FeatureID")
+}
+
+
 ################################################################################################
 # Make pairs for stat_compare_means  
 # Handy script for make comparison paris to use with ggpubr::stat_compare_means()
@@ -116,7 +126,7 @@ calculate_gunifrac_anosim <- function(ps, dist, comparisons){
     #sample_sel[1:10]
     ps.com <- prune_samples(sample_sel, ps.x)
     
-   # ps.com <- subset_samples(ps.x, condition_status %in% comps_grp[[i]])
+    # ps.com <- subset_samples(ps.x, condition_status %in% comps_grp[[i]])
     
     ps.com <- prune_taxa((taxa_sums(ps.com) >0), ps.com)
     
@@ -148,7 +158,7 @@ plot_ordination_gg <- function(ps, ord){
   evals1 <- round(ord$values$Eigenvalues[1] / sum(ord$values$Eigenvalues) * 100, 2)
   evals2 <- round(ord$values$Eigenvalues[2] / sum(ord$values$Eigenvalues) * 100, 2)
   
-  plot_ord_1 <- ggplot(ordip, aes(x = Axis.1, y = Axis.2)) + 
+  plot_ord_1 <- ggplot(ordip, aes(x = Axis.1, y = Axis.2,color = as.factor(condition_status))) + 
     # add layers
     # add lines
     geom_vline(xintercept = 0, alpha=0.5) + 
@@ -157,6 +167,43 @@ plot_ordination_gg <- function(ps, ord){
     geom_point(aes(color = as.factor(condition_status)), 
                size = 1, alpha=0.5) +
     # add gradient colors
+    scale_color_manual("",values = dis_col) + 
+    # add x and y labels
+    xlab(paste("PCoA 1 (", evals1, "%)", sep = "")) +
+    ylab(paste("PCoA 2 (", evals2, "%)", sep = "")) +
+    theme_biome_utils() #labs(title = "ASV level") 
+}
+
+
+################################################################################################
+
+################################################################################################
+
+plot_centroids <- function(ps, ord){
+  
+  ordip <- plot_ordination(ps, ord, justDF = T)
+  # Get axis 1 and 2 variation
+  evals1 <- round(ord$values$Eigenvalues[1] / sum(ord$values$Eigenvalues) * 100, 2)
+  evals2 <- round(ord$values$Eigenvalues[2] / sum(ord$values$Eigenvalues) * 100, 2)
+  
+  cents <- ordip %>% 
+    group_by(condition_status) %>% 
+    summarise(Axis.1a = mean(Axis.1),
+              Axis.2a = mean(Axis.2),
+              ster.1 = std.erF(Axis.1),
+              ster.2 = std.erF(Axis.2))
+  
+  plot_ord_1 <- ggplot(cents, 
+                       aes(x = Axis.1a, y = Axis.2a,
+                           color = as.factor(condition_status))) + 
+    geom_vline(xintercept = 0, alpha=0.5) + 
+    geom_hline(yintercept = 0, alpha=0.5) +
+    # add points
+    geom_point(data=cents,aes(color = as.factor(condition_status)), 
+               size = 3, alpha=0.5) +
+    geom_errorbarh(data=cents,aes(xmin=Axis.1a-ster.1,xmax=Axis.1a+ster.1),width=0.1) +
+    geom_errorbar(data=cents,aes(ymin=Axis.2a-ster.2,ymax=Axis.2a+ster.2),height=0.1) +
+    #geom_errorbar(data=cents,aes(ymin=Axis.2a-ster.2,ymax=Axis.2a+ster.2),height=0.1) +
     scale_color_manual("",values = dis_col) + 
     # add x and y labels
     xlab(paste("PCoA 1 (", evals1, "%)", sep = "")) +
@@ -339,4 +386,124 @@ taxa_pv_boot <- function(pseq, lower.conf=0.025, upper.conf=0.975, bs.iter=99){
 }
 
 
+#' @title Heatmap using \code{\link{phyloseq-class}} and \code{\link{pheatmap}}
+#' @description Plot heatmap using \code{\link{phyloseq-class}} object as input.
+#' @param x \code{\link{phyloseq-class}} object.
+#' @param subset.top either NA or number of Top OTUs to use for plotting.
+#' @param transformation either 'log10', 'clr','Z', 'compositional', or NA
+#' @param VariableA main variable of Interest.
+#' @param heatcolors is the option for colors in \code{\link{pheatmap}}. Default is to use Spectral
+#' @param ... Arguments to be passed \code{\link{pheatmap}}.
+#' @return A \code{\link{pheatmap}} plot object.
+#' @export
+#' @importFrom pheatmap pheatmap
+#' @importFrom RColorBrewer brewer.pal
+#' @author Sudarshan A. Shetty (sudarshanshetty9@gmail.com)
+#' @examples
+#'
+#' library(microbiomeutilities)
+#' library(viridis)
+#' library(RColorBrewer)
+#' data("zackular2014")
+#' ps0 <- zackular2014
+#'
+#' heat.sample <- plot_taxa_heatmap(ps0,
+#'   subset.top = 20,
+#'   VariableA = "DiseaseState",
+#'   heatcolors = colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(100),
+#'   transformation = "log10"
+#' )
+#' @keywords visualization
+plot_taxa_heatmap_2 <- function(x, subset.top, 
+                                transformation,
+                                VariableA, 
+                                heatcolors = NULL, ...) {
+  topOTU <- phyobj1 <- phyobj2 <- otu.mat <- NULL
+  meta.tab <- select.meta <- color.heatmap <- NULL
+  
+  
+  #x <- ps0
+  
+  x <- suppressWarnings(suppressMessages(format_to_besthit(x)))
+  if (!is.null(subset.top)) {
+    message(paste0("Top ", subset.top, " OTUs selected",
+                   sep = " "
+    ))
+    topOTU <- top_taxa(x, n = subset.top)
+  } else {
+    stop("specify a number/value for subset.top")
+  }
+  
+  prev.tx <- prevalence(x)[topOTU]
+  
+  if (transformation == "log10") {
+    message("log10, if zeros in data then log10(1+x) will be used")
+    phyobj1 <- prune_taxa(topOTU, x)
+    message("First top taxa were selected and \nthen abundances tranformed to log10(1+X)")
+    
+    phyobj2 <- microbiome::transform(phyobj1, "log10")
+  } else if (transformation == "compositional") {
+    phyobjx <-  microbiome::transform(x, "compositional")
+    phyobj2 <- prune_taxa(topOTU, phyobjx)
+    
+    #prev.tx <- prevalence(phyobj2)
+    
+    message("First converted to compositional \n then top taxa were selected")
+  } else if (transformation == "Z-OTU") {
+    phyobj1 <- prune_taxa(topOTU, x)
+    
+    #prev.tx <- prevalence(phyobj1)
+    
+    phyobj2 <-  microbiome::transform(phyobj1, "Z")
+    message("First top taxa were selected and \nthen abundances tranformed to Z values")
+  } else if (transformation == "clr") {
+    phyobj1 <- prune_taxa(topOTU, x)
+    
+    #prev.tx <- prevalence(phyobj1)
+    
+    phyobj2 <-  microbiome::transform(phyobj1, "clr")
+    message("First top taxa were selected and \nthen abundances tranformed to clr")
+  } else if (!is.null(transformation)) {
+    stop("specify a number for transformation, log10, compositional, Z-OTU, clr")
+  }
+  
+  
+  # format the taxonomy to incluse unique names
+  # phyobj2 <- format_phyloseq(phyobj2)
+  
+  
+  otu.mat <- abundances(phyobj2)
+  meta.tab <- meta(phyobj2)
+  
+  # choose which variables of interest to include in
+  # the heatmap
+  select.meta <- subset(meta.tab, select = c(VariableA))
+  
+  
+  if (is.null(heatcolors)) {
+    color.heatmap <- brewer.pal(6, "Spectral")
+  } else {
+    color.heatmap <- heatcolors
+  }
+  
+  newnames <- NULL
+  newnames <- lapply(
+    rownames(otu.mat),
+    function(x) bquote(italic(.(x))))
+  #row_df <- NULL
+  #row_df <- as.data.frame(round(prev.tx*100, 2))
+  #colnames(row_df) <- c("Prevalence")
+  
+  
+  heatmap <- pheatmap::pheatmap(otu.mat,
+                                labels_row = as.expression(newnames),
+                                annotation_col = select.meta,
+                                #annotation_colors = annotation_colors,
+                                #annotation_row = row_df,
+                                color = color.heatmap, ...)
+  
+  
+  return(list("plot"=heatmap, "tax_tab"=otu.mat, "prev" = prev.tx))
+  
+}
 
